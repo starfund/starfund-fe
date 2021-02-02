b3ce10192d7855a207fe8164bb8165bf11a8aca0
fedeagripa <federicogagripa@gmail.com>
2021-02-02 13:50:51 -0300

    loop

--- src/components/ParalaxHome.js
+++ src/components/ParalaxHome.js
@@ -5,7 +5,7 @@ import './index.css';
 const ParalaxHome = () => {
   return (
     <div className="parallax">
-      <video autoPlay src={require('../assets/HomeVid.mp4')}>
+      <video autoPlay loop src={require('../assets/HomeVid.mp4')}>
         <track src="captions_en.vtt" kind="captions" srcLang="en" label="english_captions" />
       </video>
     </div>
