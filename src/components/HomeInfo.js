989fd4d29b96b42ba0382cc02b4c64d2c7aa5b99
fedeagripa <federicogagripa@gmail.com>
2021-02-05 03:33:36 -0300

    first look of homepage

--- src/components/HomeInfo.js
+++ src/components/HomeInfo.js
@@ -2,14 +2,47 @@ import React from 'react';
 
 const HomeInfo = () => {
   return (
-    <div>
-      <h1> What is Starfund? </h1>
-      <h4>
-        {' '}
-        Starfund is a place where Stars create exclusive content, products, and surprises for their
-        fans. Fans can become an active participant in the work of their favorite idols, in the
-        monthly membership form.{' '}
-      </h4>
+    <div className="info-container">
+      <h1> WHAT`S STARFUND? </h1>
+      <div>
+        <h4>
+          {' '}
+          Starfund is a place where Stars create exclusive content, products, and surprises for
+          their fans. Fans can become an active participant in the work of their favorite idols, in
+          the monthly membership form.{' '}
+        </h4>
+      </div>
+      <div>
+        <h1> HOW DOES IT WORK? </h1>
+        <h4>
+          {' '}
+          Starfund powers membership business for athletes by giving them the tools they need to
+          acquire manage and energize their paying fans, with a subscription-style payment model,
+          fans pay their favorite athletes a monthly amount of their choice in exchange for
+          exclusive, extra content, or a closer look into their athletic life.{' '}
+        </h4>
+        <div className="steps">
+          <div className="step1">
+            <h3> STEP 1 </h3>
+            <h2> Pick Your Favorite Fighter </h2>
+            <h5> Let them know that, they are not alone and you got their back. </h5>
+          </div>
+          <div className="step2">
+            <h3> STEP 2 </h3>
+            <h2> Pick The Amount </h2>
+            <h5> Pick the existing membership of your favorite fighter. </h5>
+          </div>
+          <div className="step3">
+            <h3> STEP 3 </h3>
+            <h2> Enjoy Exclusive Access </h2>
+            <h5> Be a part of the Journey from the Beginning Experience exclusive access. </h5>
+          </div>
+        </div>
+        <h2> Are you ready to connect with your Stars? </h2>
+        <button type="button" className="btn btn-lg btn-danger">
+          GET STARTED
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/HomeStars.js
+++ src/components/HomeStars.js
@@ -4,6 +4,16 @@ const HomeStars = () => {
   return (
     <div>
       <h1> STARFUND STARS </h1>
+      <div className="fighters-container">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(f => (
+          <div key={f} className="card fighter-card">
+            <img className="card-img-top" src="..." alt="Card cap" />
+            <div className="card-body">
+              <p className="card-text">ASDF movie</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
