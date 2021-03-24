3307fea3e0aa0e94be57f8586db6f7cb8d96582b
fedeagripa <federicogagripa@gmail.com>
2021-03-24 16:09:21 -0300

    add watch videos

--- src/pages/WatchPage.js
+++ src/pages/WatchPage.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getFighters } from '../state/actions/fighterActions';
 
 const WatchPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getFighters());
+  }, [dispatch]);
+  const fighters = useSelector(state => state.fighters.fighters);
+  const [url, setUrl] = useState(fighters[0]?.publicVideos[0]?.url);
+
   return (
     <div className="watch-container">
-      <p> Coming soon... </p>
+      <h1> Discover Athletes </h1>
+      <br />
+      <video width="800" height="450" controls src={url} />
+      <div className="flex">
+        {fighters.length > 0 &&
+          fighters.map(
+            f =>
+              f.publicVideos && (
+                <div
+                  key={f.id}
+                  className="col-sm-4 fighter-watch"
+                  onClick={() => setUrl(f.publicVideos[0]?.url)}
+                >
+                  <video width="300" height="250">
+                    <source src={f.publicVideos[0]?.url} />
+                  </video>
+                </div>
+              )
+          )}
+      </div>
     </div>
   );
 };
