1b77971f7c65dfdabd978511fb6a8084780a6151
fedeagripa <federicogagripa@gmail.com>
2021-06-21 01:39:12 -0400

    fix dream post

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -2,7 +2,7 @@ import httpClient from 'httpClient';
 
 class TransactionService {
   static requestDream(dream) {
-    return httpClient.post('/petitions/dream', dream);
+    return httpClient.post('/petitions/dream', { dream });
   }
 }
 
