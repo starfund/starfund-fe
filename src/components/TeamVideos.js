cbc5d1c13fdf80cb102a6f5e440682596f8e70e8
fedeagripa <federicogagripa@gmail.com>
2021-07-19 04:10:46 -0400

    revert flat()

--- src/components/TeamVideos.js
+++ src/components/TeamVideos.js
@@ -158,7 +158,6 @@ const TeamVideos = ({ team, supporting, subscribeAction }) => {
           {team.fighters
             .map(f => f.privateVideos)
             ?.filter((_, v) => v != [])
-            .flat()
             .filter(c => !!c.video).length > 0 &&
             !payedTeam.includes(team.id) && (
               <div className={`other-videos ${isMobile && 'center'}`}>
@@ -170,7 +169,6 @@ const TeamVideos = ({ team, supporting, subscribeAction }) => {
                         videos: team.fighters
                           .map(f => f.privateVideos)
                           ?.filter((_, v) => v != [])
-                          .flat()
                           .filter(c => !!c.video).length
                       }}
                     />
