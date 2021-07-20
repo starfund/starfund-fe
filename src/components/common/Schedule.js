bc6aeaf03d188beef409c192aae8fac1405c5442
fedeagripa <federicogagripa@gmail.com>
2021-07-20 12:11:16 -0400

    fix gym schedule

--- src/components/common/Schedule.js
+++ src/components/common/Schedule.js
@@ -7,6 +7,22 @@ const Schedule = ({ title }) => {
       <h2> {title} </h2>
       <br />
       <table className="table">
+        <tbody className="table-light">
+          <tr>
+            <td> Musculacion </td>
+            <td className="table-warning"> Lun a Vie 8:00 - 12:00 </td>
+          </tr>
+          <tr>
+            <td />
+            <td className="table-warning"> Lun a Vie 15:00 - 21:00 </td>
+          </tr>
+          <tr>
+            <td />
+            <td className="table-warning"> Sab 9:00 - 12:00 </td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="table">
         <thead className="table-light">
           <tr>
             <th />
@@ -15,66 +31,57 @@ const Schedule = ({ title }) => {
             <th> Mie </th>
             <th> Jue </th>
             <th> Vie </th>
-            <th> Sab </th>
-            <th> Dom </th>
           </tr>
         </thead>
         <tbody className="table-light">
           <tr>
-            <th scope="row">9:00 - 10:00</th>
-            <td className="table-primary">Fighting</td>
+            <th scope="row">
+              8:00 <br /> - <br /> 9:00
+            </th>
+            <td className="table-info">Funcional</td>
             <td />
+            <td className="table-info">Funcional</td>
             <td />
-            <td />
-            <td className="table-primary">Fighting</td>
-            <td />
-            <td />
-          </tr>
-          <tr>
-            <th scope="row">10:00 - 11:00</th>
-            <td />
-            <td />
-            <td className="table-primary">Fighting</td>
-            <td />
-            <td />
-            <td />
-            <td />
+            <td className="table-info">Funcional</td>
           </tr>
           <tr>
             <th scope="row" className="table-secondary" />
-            {[1, 2, 3, 4, 5, 6, 7].map(() => (
+            {[1, 2, 3, 4, 5].map(() => (
               <td className="table-secondary" />
             ))}
           </tr>
           <tr>
-            <th scope="row">16:00 - 17:00</th>
-            <td className="table-warning">Open Gym</td>
-            <td />
-            <td />
-            <td className="table-success"> Fittness</td>
-            <td />
-            <td />
-            <td />
+            <th scope="row">
+              18:00
+              <br /> - <br /> 19:00
+            </th>
+            <td className="table-info"> FULL BODY</td>
+            <td className="table-danger"> POWER FUNCIONAL</td>
+            <td className="table-info"> FULL BODY</td>
+            <td className="table-danger"> POWER FUNCIONAL</td>
+            <td className="table-info"> FULL BODY</td>
           </tr>
           <tr>
-            <th scope="row">17:00 - 18:00</th>
-            <td />
-            <td />
-            <td />
-            <td />
-            <td />
-            <td />
-            <td />
+            <th scope="row">
+              19:00
+              <br /> - <br /> 20:00
+            </th>
+            <td className="table-violet"> POWER HIT</td>
+            <td className="table-orange"> POWER CROSS</td>
+            <td className="table-violet"> POWER HIT</td>
+            <td className="table-orange"> POWER CROSS</td>
+            <td className="table-violet"> POWER HIT</td>
           </tr>
           <tr>
-            <th scope="row">18:00 - 19:00</th>
+            <th scope="row">
+              19:00
+              <br /> - <br /> 20:00
+            </th>
+            <td className="table-success"> THAI BOXING</td>
             <td />
+            <td className="table-success"> THAI BOXING</td>
             <td />
-            <td />
-            <td />
-            <td />
-            <td />
-            <td />
+            <td className="table-success"> THAI BOXING</td>
           </tr>
         </tbody>
       </table>
