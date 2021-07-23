20af34c38902558a9e12d162af3f98f6dea6d890
fedeagripa <federicogagripa@gmail.com>
2021-07-22 23:41:37 -0400

    subscribe to gyms

--- src/hooks/usePayments.js
+++ src/hooks/usePayments.js
@@ -12,7 +12,7 @@ export default stripe => {
   const { showErrorToast } = useToast();
 
   const createCreditCard = async billing => {
-    const { name, email, fighter, team, type } = billing;
+    const { name, email, fighter, team, business, type } = billing;
 
     setLoading(true);
 
@@ -38,7 +38,7 @@ export default stripe => {
       };
       if (type === 'subscription') {
         await dispatch(
-          subscribe({ token: data.token, name, email, fighter, team, card_data: data })
+          subscribe({ token: data.token, name, email, fighter, team, business, card_data: data })
         );
       } else {
         await dispatch(charge({ token: data.token, name, email, fighter, card_data: data }));
