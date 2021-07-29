bd804c9be1e888ead9c50c84ca20ef4f3a866ce3
fedeagripa <federicogagripa@gmail.com>
2021-07-29 13:54:43 -0400

    fix table styles

--- src/components/common/Schedule.js
+++ src/components/common/Schedule.js
@@ -6,24 +6,25 @@ const Schedule = ({ title }) => {
     <div className="schedule">
       <h2> {title} </h2>
       <br />
-      <table className="table">
-        <tbody className="table-light">
+      <table className="table table-dark table-borderless">
+        <tbody>
           <tr>
             <td />
-            <td className="table-warning"> Lun a Vie 8:00 - 12:00 </td>
+            <td className=""> Lun a Vie 8:00 - 12:00 </td>
           </tr>
           <tr>
             <td> Musculacion </td>
-            <td className="table-warning"> Lun a Vie 15:00 - 21:00 </td>
+            <td className=""> Lun a Vie 15:00 - 21:00 </td>
           </tr>
           <tr>
             <td />
-            <td className="table-warning"> Sab 9:00 - 12:00 </td>
+            <td className=""> Sab 9:00 - 12:00 </td>
           </tr>
         </tbody>
       </table>
-      <table className="table">
-        <thead className="table-light">
+      <br />
+      <table className="table table-striped">
+        <thead className="thead-light">
           <tr>
             <th />
             <th> Lun </th>
@@ -33,16 +34,14 @@ const Schedule = ({ title }) => {
             <th> Vie </th>
           </tr>
         </thead>
-        <tbody className="table-light">
+        <tbody className="table-dark">
           <tr>
-            <th scope="row">
-              8:00 <br /> - <br /> 9:00
-            </th>
-            <td className="table-info">Funcional</td>
+            <th scope="row">8:00 - 9:00</th>
+            <td className="">Funcional</td>
             <td />
-            <td className="table-info">Funcional</td>
+            <td className="">Funcional</td>
             <td />
-            <td className="table-info">Funcional</td>
+            <td className="">Funcional</td>
           </tr>
           <tr>
             <th scope="row" className="table-secondary" />
@@ -51,37 +50,28 @@ const Schedule = ({ title }) => {
             ))}
           </tr>
           <tr>
-            <th scope="row">
-              18:00
-              <br /> - <br /> 19:00
-            </th>
-            <td className="table-info"> FULL BODY</td>
-            <td className="table-danger"> POWER FUNCIONAL</td>
-            <td className="table-info"> FULL BODY</td>
-            <td className="table-danger"> POWER FUNCIONAL</td>
-            <td className="table-info"> FULL BODY</td>
+            <th scope="row">18:00 - 19:00</th>
+            <td className=""> FULL BODY</td>
+            <td className=""> POWER FUNCIONAL</td>
+            <td className=""> FULL BODY</td>
+            <td className=""> POWER FUNCIONAL</td>
+            <td className=""> FULL BODY</td>
           </tr>
           <tr>
-            <th scope="row">
-              19:00
-              <br /> - <br /> 20:00
-            </th>
-            <td className="table-violet"> POWER HIT</td>
-            <td className="table-orange"> POWER CROSS</td>
-            <td className="table-violet"> POWER HIT</td>
-            <td className="table-orange"> POWER CROSS</td>
-            <td className="table-violet"> POWER HIT</td>
+            <th scope="row">19:00 - 20:00</th>
+            <td className=""> POWER HIT</td>
+            <td className=""> POWER CROSS</td>
+            <td className=""> POWER HIT</td>
+            <td className=""> POWER CROSS</td>
+            <td className=""> POWER HIT</td>
           </tr>
           <tr>
-            <th scope="row">
-              19:00
-              <br /> - <br /> 20:00
-            </th>
-            <td className="table-success"> THAI BOXING</td>
+            <th scope="row">19:00 - 20:00</th>
+            <td className=""> THAI BOXING</td>
             <td />
-            <td className="table-success"> THAI BOXING</td>
+            <td className=""> THAI BOXING</td>
             <td />
-            <td className="table-success"> THAI BOXING</td>
+            <td className=""> THAI BOXING</td>
           </tr>
         </tbody>
       </table>
