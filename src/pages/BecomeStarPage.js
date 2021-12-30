0c88057e737f33115d2dfc907f7c9ca8047c3bb0
Juli <jdubra@starfund.app>
2021-12-30 15:03:20 -0300

    home page notification sizes

--- src/pages/BecomeStarPage.js
+++ src/pages/BecomeStarPage.js
@@ -1,34 +1,11 @@
-import React, { useState } from 'react';
-import { useIntl } from 'react-intl';
-import cn from 'classnames';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
 const BecomeStarPage = () => {
-  const intl = useIntl();
-  const [isStar, setIsStar] = useState();
   return (
     <div className="contact-container">
       <div className="container">
         <div className="row center">
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="navbar-collapse" id="navbarText">
-              <React.Fragment>
-                <ul className="navbar-nav mr-auto">
-                  <li className={cn('nav-item', { active: isStar })}>
-                    <Link className="nav-link" href="" onClick={() => setIsStar(true)}>
-                      {intl.formatMessage({ id: 'enroll.star' })}{' '}
-                      <span className="sr-only">(current)</span>
-                    </Link>
-                  </li>
-                  <li className={cn('nav-item', { active: !isStar })}>
-                    <Link className="nav-link" onClick={() => setIsStar(false)}>
-                      {intl.formatMessage({ id: 'enroll.organization' })}
-                    </Link>
-                  </li>
-                </ul>
-              </React.Fragment>
-            </div>
-          </nav>
+          <h1>Email us: info@starfund.app</h1>
         </div>
       </div>
     </div>
